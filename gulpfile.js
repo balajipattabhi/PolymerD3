366b092af7b3176f1a5c74b78e13ab0190ea87f4
212591762 <balaji.pattabhiraman@ge.com>
2017-04-23 14:28:41 +0530

    Vulcan

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var vulcanize = require('gulp-vulcanize');
+
+gulp.task('vulcanize', function() {
+  return gulp.src(['BarChart/*.html', 'BubbleChart/*.html', 'DonutChart/*.html','GroupedBar/*.html',
+  'LiquidGauge/*.html','PieChart/*.html','ScatterPlot/*.html','StackedAreaChart/*.html','TimeSeries/*.html'])
+  .pipe(vulcanize({
+    stripComments: true,
+    inlineScripts: true,
+    inlineCss: true
+  }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['vulcanize']);
